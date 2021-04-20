ba9007d839517dc279f23850438935ae5fc79cb9
fahruraji <owatanabe82@gmail.com>
2021-04-20 12:26:40 +0800

    Percabangan IfElseIf (kondisi1 dan kondisi2 false)

--- src/percabangan/IfElse.java
+++ src/percabangan/IfElse.java
@@ -3,7 +3,7 @@ package percabangan;
 public class IfElse {
     public static void main(String[] args) {
         double totalBelanja = 25000;
-        double uangDiDompet = 50000;
+        double uangDiDompet = 25000;
 
         if(uangDiDompet<totalBelanja){
             System.out.println("Uang kurang, kurangi jajan anda");
