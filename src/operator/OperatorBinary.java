df52f1736f21471d2799db626fe21facb984101a
fahruraji <owatanabe82@gmail.com>
2021-04-20 12:02:12 +0800

    Operator Binary Assignment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assignment/penugasan
+    //menempatkan nilai ke dalam variabel
+    //baris 5-7 adalah comment/komentar yang tidak dijalankan oleh program
+    
+    String nama = "Fahruraji";
+    int jumlahSKS = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
